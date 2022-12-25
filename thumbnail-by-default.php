a42b9b55c1baf556148f6fc0f1b21751e3c9b11c
PierricM380 <pierric.marye@wanadoo.fr>
2022-12-25 16:08:34 +0100

    filtered hooks methods. First return image into HTML and second say that the post has always an image.

--- thumbnail-by-default.php
+++ thumbnail-by-default.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name:    Thumbnail By Default
- * Plugin URI:
+ * Plugin URI:     https://github.com/PierricM380/thumbnail-by-default 
  * Description:    Thumbnail By Default va vous permettra de choisir une image par défaut pour votre article.
  * Version:        1.0.0
  * Author:         LuneDev66
@@ -13,23 +13,50 @@
  */
 
 /**
- * WordPress offers filter hooks to allow plugins to modify various types of internal data at runtime
+ * post_thumbnail_html -> Filters the post thumbnail HTML
  * apply_filters( 'post_thumbnail_html', $html, $post->ID, $post_thumbnail_id, $size, $attr );
  * https://developer.wordpress.org/reference/functions/get_the_post_thumbnail/
- */ 
+ */
 add_filter('post_thumbnail_html', 'thumbnail_by_default_html', 10, 5);
 
 /**
- * Undocumented function
+ * Return post HTML with image id
  *
- * @param [type] $html
- * @param [type] $post_id
- * @param [type] $thumbnail_id
- * @param [type] $size
- * @param [type] $attr
+ * @param $html
+ * @param $post_id
+ * @param $thumbnail_id
+ * @param $size
+ * @param $attr
  * @return void
  */
 function thumbnail_by_default_html($html, $post_id, $thumbnail_id, $size, $attr)
 {
+    if ( !empty($html) && !$thumbnail_id) {
+        $html = wp_get_attachment_image(11, $size, false, $attr);
+    }
+
     return $html;
+}
+
+/**
+ * has_post_thumbnail -> Filters whether a post has a post thumbnail.
+ * apply_filter('has_post_thumbnail', $has_thumbnail, $post, $thumbnail_id)
+ */
+add_filter('has_post_thumbnail', 'thumbnail_by_default_has_post_thumbnail', 10, 3);
+
+/**
+ * Always return true for an post image
+ *
+ * @param $has_thumbnail
+ * @param $post
+ * @param $thumbnail_id
+ * @return void
+ */
+function thumbnail_by_default_has_post_thumbnail($has_thumbnail, $post, $thumbnail_id)
+{
+    if ('post' === get_post_type($post)) {
+        $has_thumbnail = true;
+    }
+
+    return $has_thumbnail;
 }
