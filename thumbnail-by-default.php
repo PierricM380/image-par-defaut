01cd136979f9ca819c3b309c6cd40198f0b55e67
PierricM380 <pierric.marye@wanadoo.fr>
2022-12-25 15:13:02 +0100

    Initialize project

---
+++ thumbnail-by-default.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:    Thumbnail By Default
+ * Plugin URI:
+ * Description:    Thumbnail By Default va vous permettra de choisir une image par défaut pour votre article.
+ * Version:        1.0.0
+ * Author:         LuneDev66
+ * Author URI:     https://pierricmaryedeveloppeurweb.netlify.app/
+ * Livence:        GPL v2 or later
+ * Text Domain:    thumbnail-by-default
+ * Domain Path:    /languages
+ */
+
+// apply_filters( 'post_thumbnail_html', $html, $post->ID, $post_thumbnail_id, $size, $attr );
+
