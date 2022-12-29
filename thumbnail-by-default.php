d56547e073f42465ec0d00d5b0cf362100606633
PierricM380 <pierric.marye@wanadoo.fr>
2022-12-29 10:10:32 +0100

    Plugin Name changed. News comments. Adding section into configuration them panel. Improve code

--- thumbnail-by-default.php
+++ thumbnail-by-default.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * Plugin Name:    Thumbnail By Default
+ * Plugin Name:    Image Par Défaut
  * Plugin URI:     https://github.com/PierricM380/thumbnail-by-default 
- * Description:    Thumbnail By Default va vous permettra de choisir une image par défaut pour votre article.
+ * Description:    Image par défaut va vous permettra de choisir une image pour vos articles. Rendez-vous dans l'onglet "Apparence" -> "Personnaliser" -> 
  * Version:        1.0.0
  * Author:         LuneDev66
  * Author URI:     https://pierricmaryedeveloppeurweb.netlify.app/
@@ -20,21 +20,23 @@
 add_filter('post_thumbnail_html', 'thumbnail_by_default_html', 10, 5);
 
 /**
- * Return post HTML with image id
+ * Return post HTML with default image
  *
- * @param $html
- * @param $post_id
- * @param $thumbnail_id
- * @param $size
- * @param $attr
- * @return void
+ * @param   string  $html
+ * @param   int     $post_id
+ * @param   int     $thumbnail id
+ * @param   string  $size
+ * @param   array   $attributes
+ * @return  string  $html
+ * 
  */
 function thumbnail_by_default_html($html, $post_id, $thumbnail_id, $size, $attr)
 {
-    if (!empty($html) && !$thumbnail_id) {
-        $html = wp_get_attachment_image(11, $size, false, $attr);
+    $post_type = get_post_type($post_id);
+    if (empty($html) && !$thumbnail_id && in_array($post_type, array('post', 'page'))) {
+        $default_id = get_option("default_${post_type}_thumbnail");
+        $html       = wp_get_attachment_image($default_id, $size, false, $attr);
     }
-
     return $html;
 }
 
@@ -47,17 +49,17 @@ add_filter('has_post_thumbnail', 'thumbnail_by_default_has_post_thumbnail', 10, 
 /**
  * Always return true for an post image. Work only for articles.
  *
- * @param $has_thumbnail
- * @param $post
- * @param $thumbnail_id
- * @return void
+ * @param bool             $has_thumbnail
+ * @param int|WP_Post|null $post
+ * @param int|false        $thumbnail_id
+ * 
  */
 function thumbnail_by_default_has_post_thumbnail($has_thumbnail, $post, $thumbnail_id)
 {
-    if ('post' === get_post_type($post)) {
-        $has_thumbnail = true;
+    $post_type = get_post_type($post);
+    if (!$thumbnail_id && in_array($post_type, array('post', 'page'))) {
+        return (bool) get_option("default_${post_type}_thumbnail");
     }
-
     return $has_thumbnail;
 }
 
@@ -67,24 +69,33 @@ function thumbnail_by_default_has_post_thumbnail($has_thumbnail, $post, $thumbna
  * Use instance of customize_register hook
  */
 
-add_action('customize_register', 'thumbnail_by_default_customize_register', 10, 1);
+add_action('customize_register', 'thumbnail_by_default_customize_register');
+/**
+ * Add parameters options into theme configuration panel
+ *
+ * @param $wp_customize
+ */
 function thumbnail_by_default_customize_register($wp_customize)
 {
-    $wp_customize->add_section( 'thumbnail_by_default_settings', array(
-        'title' => __( 'Thumbnail By Default', 'thumbnail-by-default' ),
-        'description' => __( 'Ajouter du CSS ici', 'thumbnail-by-default' ),
-      ) );
-
-      $wp_customize->add_setting( 'thumbnail_by_default_id', array(
-        'type' => 'option', // or 'theme_mod'
-        'default' => '',
+    $wp_customize->add_section('thumbnail', array(
+        'title' => __('Image par défaut', 'thumbnail-by-default'),
+    ));
+    $wp_customize->add_setting('default_post_thumbnail', array(
+        'type'              => 'option',
         'sanitize_callback' => 'absint',
-      ) );
-
-      $wp_customize->add_control( 'thumbnail_by_default_id', array(
-        'type' => 'number',
-        'section' => 'thumbnail_by_default_settings',
-        'label' => __( 'Image par défaut des articles', 'thumbnail-by-default' ),
-        'description' => __( 'Entrer un identifiant de l\'image par défaut.', 'thumbnail-by-default' ),
-      ) );
+    ));
+    $wp_customize->add_control(new WP_Customize_Media_Control($wp_customize, 'default_post_thumbnail', array(
+        'label'     => __('Choisir une image pour vos articles', 'thumbnail-by-default'),
+        'section'   => 'thumbnail',
+        'mime_type' => 'image',
+    )));
+    $wp_customize->add_setting('default_page_thumbnail', array(
+        'type'              => 'option',
+        'sanitize_callback' => 'absint',
+    ));
+    $wp_customize->add_control(new WP_Customize_Media_Control($wp_customize, 'default_page_thumbnail', array(
+        'label'     => __('Choisir une image pour vos pages', 'thumbnail-by-default'),
+        'section'   => 'thumbnail',
+        'mime_type' => 'image',
+    )));
 }
